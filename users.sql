03aaa75ab99cff37ca2885bf714c7762215aa4b9
Nicholas Guyaux <nicholas.guyaux@gmail.com>
2016-11-20 21:18:42 -0700

    saving work

---
+++ users.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS sql-challenge;
+CREATE DATABASE sql-challenge;
+
+\c sql-challenge;
+
+CREATE TABLE pups (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR,
+  breed VARCHAR,
+  age INTEGER,
+  sex VARCHAR
+);
+
+INSERT INTO pups (name, breed, age, sex)
+  VALUES ('Tyler', 'Retrieved', 3, 'M');
